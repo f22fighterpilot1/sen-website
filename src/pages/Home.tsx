ee2cb56332a910f842579ba739e542d170af8952
Drew Irechukwu <dr.druc4o5@yahoo.com>
2025-12-25 03:09:05 -0500

    updated the worlds first... lines and styling on Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,7 +34,13 @@ export default function Home() {
                         Perceptual AI Infrastructure • Deterministic • Explainable
                     </div>
 
-                    <h1>The world’s first fully deterministic AI system.</h1>
+                   <h1>
+                    The world’s first<br />
+                    fully deterministic<br />
+                    AI Infrastructure
+                    </h1>
+
+
 
                     <p className="lead">
                         SymbolicEngine (SEN) is a geometry-aware perceptual system that recognizes symbols by
